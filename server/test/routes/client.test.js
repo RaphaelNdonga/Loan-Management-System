1b715152087975a21d9a5808b99eb9c3a9e335ef
RaphaelNdonga <nkibi53@gmail.com>
2024-08-08 09:45:32 +0300

    client functionality tested end to end

--- server/test/routes/client.test.js
+++ server/test/routes/client.test.js
@@ -2,8 +2,46 @@ import request from "supertest"
 describe("Client functionality test", function () {
     const testUrl = "http://localhost:8000"
     let client
+    let adminUser
     let token
 
+    test("register endpoint 200 (ok)", function (done) {
+        const user = {
+            firstname: "ClientTester",
+            lastname: "McClient",
+            contactNumber: "123456789",
+            address: "Testing environment",
+            email: "clientester@test.com",
+            username: "clienttester01",
+            password: "password",
+        }
+        request(testUrl)
+            .post("/register")
+            .send(user)
+            .expect(200)
+            .end(function (err, res) {
+                if (err) {
+                    console.log("Error in /register", err)
+                    return done(err)
+                }
+                token = res.body["token"]
+                return done()
+            })
+    })
+    test("gets profile", function (done) {
+        request(testUrl)
+            .get("/profile")
+            .set("Authorization", token)
+            .expect(200)
+            .end(function (err, res) {
+                if (err) {
+                    console.log("Error in /profile", err)
+                    return done(err)
+                }
+                adminUser = res.body
+                return done()
+            })
+    })
     test("Adds client", function (done) {
         const testUser = {
             firstname: "Tester",
@@ -29,6 +67,23 @@ describe("Client functionality test", function () {
                 return done()
             })
     })
+
+    test("Gets all clients", function (done) {
+        console.log("Token: ", token)
+        request(testUrl)
+            .get("/allClients")
+            .set("Authorization", token)
+            .expect(200)
+            .end(function (err, res) {
+                if (err) {
+                    console.log("Error get /allClients", err)
+                    return done(err)
+                }
+                expect(res.body.length).toBeGreaterThan(0)
+                return done()
+            })
+    })
+
     test("Deletes client", function (done) {
         request(testUrl)
             .delete(`/clients/${client.id}`)
@@ -42,4 +97,17 @@ describe("Client functionality test", function () {
             })
     })
 
+    test("deletes admin", function (done) {
+        request(testUrl)
+            .delete(`/admin/${adminUser.id}`)
+            .expect(200)
+            .end(function (err, res) {
+                if (err) {
+                    console.log("Error in delete /admin/:id", err)
+                    return done(err)
+                }
+                console.log("deleted id: ", adminUser.id)
+                return done()
+            })
+    })
 })
